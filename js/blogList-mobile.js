e56e6d2114cce4cd79d98b3c25d7a8fea63da86c
Zeinab <zeinabansarian6280@gmail.com>
2024-12-08 17:10:45 +0330

    complete blog list page

---
+++ js/blogList-mobile.js
@@ -0,0 +1,30 @@
+
+let filterBtn = document.querySelectorAll(".filterBtn")
+let loaderContainer = document.querySelector(".loaderContainer")
+filterBtn.forEach(element => {
+element.addEventListener("click" , function (params) {
+  $(".filterBtn").removeClass("activeFilter")
+  element.classList.add("activeFilter")
+  
+})
+});
+
+
+$(document).ready(function () {
+  $(".filterBtn").click(function (e) {
+      var url4 = $(this).attr("data-blog");
+      // $(".listContainer").load(url4);
+
+    loaderContainer.style.display="flex"
+      $.ajax({
+        url: url4,
+        method: 'GET',
+        success: function(response) {
+            // مخفی کردن لودینگ بعد از دریافت پاسخ
+            $(".blogContainer").html(response);
+             loaderContainer.style.display="none"
+        },
+       
+    });
+  });
+});
