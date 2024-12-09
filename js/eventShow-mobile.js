f50e6b507a7eb1cddfe963916cb9a06b83133e78
Zeinab <zeinabansarian6280@gmail.com>
2024-12-09 14:48:42 +0330

    complete event show page

---
+++ js/eventShow-mobile.js
@@ -0,0 +1,12 @@
+const gallerySlider = new Swiper('.gallerySlider', {
+    speed:1000,
+    loop: true,
+  
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.nextGallery',
+      prevEl: '.prevGallery',
+    },
+  
+  });
